7e433d7e2890c300950fc7deaff3ec4308eab3a7
Andy Goryachev <andy.goryachev@oracle.com>
2022-08-08 15:42:35 -0700

    show last column checkbox

--- src/goryachev/apps/FitToWidth.java
+++ src/goryachev/apps/FitToWidth.java
@@ -1,11 +1,15 @@
 package goryachev.apps;
+import java.util.List;
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.skin.TableViewSkin;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -33,38 +37,51 @@ JDK-8090167 TableView not layouted correctly in scene
 JDK-8087673 [TableView] TableView and TreeTableView menu button overlaps columns when using a constrained resize policy.
 */
 public class FitToWidth extends Application {
+    protected TableView<Entry> table;
+    protected TableColumn lastColumn;
+    
     public static void main(String[] args) {
         Application.launch(FitToWidth.class, args);
     }
 
     @Override
-    public void start(Stage s) throws Exception {
-        TableView<Entry> v = new TableView();
-        v.getSelectionModel().setCellSelectionEnabled(true);
-        v.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        v.setSkin(new TableViewSkin(v));
-        v.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    public void start(Stage stage) throws Exception {
+        table = new TableView();
+        table.getSelectionModel().setCellSelectionEnabled(true);
+        table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        table.setSkin(new TableViewSkin(table));
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         
         {
             TableColumn<Entry,String> c = new TableColumn<>("Name");
-            c.setPrefWidth(100);
+            c.setPrefWidth(50);
             c.setCellValueFactory((f) ->
             {
                 return f.getValue().name;
             });
-            v.getColumns().add(c);
+            table.getColumns().add(c);
         }
         {
             TableColumn<Entry,String> c = new TableColumn("Text");
-            c.setPrefWidth(1000);
+            c.setPrefWidth(200);
             c.setCellValueFactory((f) ->
             {
                 return f.getValue().text;
             });
-            v.getColumns().add(c);
+            table.getColumns().add(c);
+        }
+        {
+            TableColumn<Entry,String> c = new TableColumn("Hash");
+            lastColumn = c;
+            c.setPrefWidth(300);
+            c.setCellValueFactory((f) ->
+            {
+                return new ReadOnlyStringWrapper(String.valueOf(f.getValue().hashCode()));
+            });
+            table.getColumns().add(c);
         }
         
-        v.getItems().addAll(
+        table.getItems().addAll(
                 new Entry("1", "One"),
                 new Entry("2", "Two"),
                 new Entry("3", "Three"),
@@ -72,9 +89,41 @@ public class FitToWidth extends Application {
                 new Entry("99", "Ninety Nine")
                 );
         
-        Scene sc = new Scene(v);
-        s.setScene(sc);
-        s.show();
+        CheckBox showLastColumnCheckbox = new CheckBox("show last column");
+        showLastColumnCheckbox.setSelected(true);
+        lastColumn.visibleProperty().bind(showLastColumnCheckbox.selectedProperty());
+        showLastColumnCheckbox.selectedProperty().addListener((src,p,c) -> {
+            dump();
+        });
+        
+        BorderPane p = new BorderPane();
+        p.setCenter(table);
+        p.setBottom(showLastColumnCheckbox);
+        
+        Scene sc = new Scene(p);
+        stage.setScene(sc);
+        stage.show();
+    }
+    
+    protected void dump() {
+        StringBuilder sb = new StringBuilder();
+        
+        List<Integer> indexes = table.getSelectionModel().getSelectedIndices();
+        sb.append("selected indexes: [");
+        
+        for(int ix: indexes) {
+            sb.append(ix).append(' ');
+        }
+        sb.append("]\n");
+        
+        sb.append("isSelected(int): [");
+        for(int i=0; i<table.getItems().size(); i++) {
+            boolean sel = table.getSelectionModel().isSelected(i);
+            sb.append(sel ? "T " : "F ");
+        }
+        sb.append("]\n");
+        
+        System.out.println(sb);
     }
     
     protected static class Entry {
