784784e3d86f877eccac2bf61290e4798632eb5f
Andy Goryachev <andy.goryachev@oracle.com>
2022-09-14 12:55:31 -0700

    8293119: fixed unconstrained label in tester

--- src/goryachev/apps/ATableViewResizeTester.java
+++ src/goryachev/apps/ATableViewResizeTester.java
@@ -1,5 +1,28 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package goryachev.apps;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -34,7 +57,7 @@ import javafx.util.Callback;
  * Tests TableView/JTable resize modes.
  */
 public class ATableViewResizeTester extends Application {
-    
+
     enum Demo {
         ALL("all set: min, pref, max"),
         PREF("pref only"),
@@ -57,20 +80,20 @@ public class ATableViewResizeTester extends Application {
         Demo(String text) { this.text = text; }
         public String toString() { return text; }
     }
-    
+
     public enum Policy {
-        AUTO_RESIZE_OFF(JTable.AUTO_RESIZE_OFF),
         AUTO_RESIZE_NEXT_COLUMN(JTable.AUTO_RESIZE_NEXT_COLUMN),
         AUTO_RESIZE_SUBSEQUENT_COLUMNS(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS),
         AUTO_RESIZE_LAST_COLUMN(JTable.AUTO_RESIZE_LAST_COLUMN),
         AUTO_RESIZE_ALL_COLUMNS(JTable.AUTO_RESIZE_ALL_COLUMNS),
-        CONSTRAINED_RESIZE_POLICY(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
-        
+        CONSTRAINED_RESIZE_POLICY(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS),
+        UNCONSTRAINED_RESIZE_POLICY(JTable.AUTO_RESIZE_OFF);
+
         private final int value;
         Policy(int v) { this.value = v; }
         public int getValue() { return value; }
     }
-    
+
     public enum Cmd {
         ROWS,
         COL,
@@ -84,7 +107,7 @@ public class ATableViewResizeTester extends Application {
     protected ComboBox<Demo> demoSelector;
     protected ComboBox<Policy> policySelector;
     protected SwingPanel swingPanel;
-    
+
     public static void main(String[] args) {
         Application.launch(ATableViewResizeTester.class, args);
     }
@@ -92,7 +115,7 @@ public class ATableViewResizeTester extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         contentPane = new BorderPane();
-        
+
         // selector
         demoSelector = new ComboBox<>();
         demoSelector.getItems().addAll(Demo.values());
@@ -100,18 +123,18 @@ public class ATableViewResizeTester extends Application {
         demoSelector.getSelectionModel().selectedItemProperty().addListener((s,p,c) -> {
             updatePane();
         });
-        
+
         policySelector = new ComboBox<>();
         policySelector.getItems().addAll(Policy.values());
         policySelector.setEditable(false);
         policySelector.getSelectionModel().selectedItemProperty().addListener((s,p,c) -> {
             updatePane();
         });
-        
+
         // layout
 
         SplitPane split = new SplitPane(contentPane, new BorderPane());
-        
+
         HBox hb = new HBox(
             new Label("Data:"),
             demoSelector,
@@ -119,30 +142,30 @@ public class ATableViewResizeTester extends Application {
             policySelector
         );
         hb.setSpacing(5);
-        
+
         BorderPane bp = new BorderPane();
         bp.setTop(hb);
         bp.setCenter(split);
-        
+
         SwingNode swn = new SwingNode();
         EventQueue.invokeLater(() -> {
             swingPanel = new SwingPanel();
             swn.setContent(swingPanel);
         });
-        
+
         SplitPane sp = new SplitPane(bp, new BorderPane(swn));
         sp.setOrientation(Orientation.VERTICAL);
-        
+
         stage.setScene(new Scene(sp));
         stage.setWidth(1000);
         stage.setHeight(500);
         stage.setTitle("TableView/JTable Resize Tester " + System.getProperty("java.version"));
         stage.show();
-        
+
         demoSelector.getSelectionModel().selectFirst();
         policySelector.getSelectionModel().selectFirst();
     }
-    
+
     protected Callback<ResizeFeatures,Boolean> wrap(Callback<ResizeFeatures,Boolean> policy) {
         return new Callback<ResizeFeatures,Boolean>() {
             @Override
@@ -150,16 +173,16 @@ public class ATableViewResizeTester extends Application {
                 Boolean rv = policy.call(f);
                 int ix = f.getTable().getColumns().indexOf(f.getColumn());
                 System.out.println(
-                    "col=" + (ix < 0 ? f.getColumn() : ix) + 
-                    " delta=" + f.getDelta() + 
-                    " w=" + f.getTable().getWidth() + 
+                    "col=" + (ix < 0 ? f.getColumn() : ix) +
+                    " delta=" + f.getDelta() +
+                    " w=" + f.getTable().getWidth() +
                     " rv=" + rv
                 );
                 return rv;
             }
         };
     }
-    
+
     protected String describe(TableColumn c) {
         StringBuilder sb = new StringBuilder();
         if(c.getMinWidth() != 10.0) {
@@ -173,7 +196,7 @@ public class ATableViewResizeTester extends Application {
         }
         return sb.toString();
     }
-    
+
     protected Callback<ResizeFeatures, Boolean> createPolicy(Policy p) {
         switch(p) {
         case AUTO_RESIZE_ALL_COLUMNS:
@@ -182,17 +205,17 @@ public class ATableViewResizeTester extends Application {
             return ConstrainedColumnResize.forTable(ResizeMode.AUTO_RESIZE_LAST_COLUMN);
         case AUTO_RESIZE_NEXT_COLUMN:
             return ConstrainedColumnResize.forTable(ResizeMode.AUTO_RESIZE_NEXT_COLUMN);
-        case AUTO_RESIZE_OFF:
-            return TableView.UNCONSTRAINED_RESIZE_POLICY;
         case AUTO_RESIZE_SUBSEQUENT_COLUMNS:
             return ConstrainedColumnResize.forTable(ResizeMode.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
         case CONSTRAINED_RESIZE_POLICY:
             return TableView.CONSTRAINED_RESIZE_POLICY;
+        case UNCONSTRAINED_RESIZE_POLICY:
+            return TableView.UNCONSTRAINED_RESIZE_POLICY;
         default:
             throw new Error("?" + p);
         }
     }
-    
+
     protected Object[] createSpec(Demo d) {
         switch(d) {
         case ALL:
@@ -346,25 +369,25 @@ public class ATableViewResizeTester extends Application {
             throw new Error("?" + d);
         }
     }
-    
+
     protected void updatePane() {
         Policy p = policySelector.getSelectionModel().getSelectedItem();
         Demo d = demoSelector.getSelectionModel().getSelectedItem();
         Object[] spec = createSpec(d);
-        
+
         Pane n = createPane(p, spec);
         contentPane.setCenter(n);
-        
+
         EventQueue.invokeLater(() -> {
             swingPanel.updatePane(p, spec);
         });
     }
-    
+
     protected void combineColumns(TableView<String> t, int ix, int count, int name) {
         TableColumn<String,String> tc = new TableColumn<>();
         tc.setText("N" + name);
-        
-        for(int i=0; i<count; i++) {
+
+        for (int i = 0; i < count; i++) {
             TableColumn<String,String> c = (TableColumn<String,String>)t.getColumns().remove(ix);
             tc.getColumns().add(c);
         }
@@ -372,24 +395,24 @@ public class ATableViewResizeTester extends Application {
     }
 
     protected Pane createPane(Policy policy, Object[] spec) {
-        if((spec == null) || (policy == null)) {
+        if ((spec == null) || (policy == null)) {
             return new BorderPane();
         }
-        
+
         TableView<String> table = new TableView();
         table.getSelectionModel().setCellSelectionEnabled(true);
         table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        
-        Callback<ResizeFeatures, Boolean> p = createPolicy(policy);
+
+        Callback<ResizeFeatures,Boolean> p = createPolicy(policy);
         table.setColumnResizePolicy(p);
 
         TableColumn<String,String> lastColumn = null;
         int id = 1;
-        
-        for(int i=0; i<spec.length; ) {
+
+        for (int i = 0; i < spec.length;) {
             Object x = spec[i++];
-            if(x instanceof Cmd cmd) {
-                switch(cmd) {
+            if (x instanceof Cmd cmd) {
+                switch (cmd) {
                 case COL:
                     TableColumn<String,String> c = new TableColumn<>();
                     table.getColumns().add(c);
@@ -397,27 +420,24 @@ public class ATableViewResizeTester extends Application {
                     c.setCellValueFactory((f) -> new SimpleStringProperty(describe(c)));
                     lastColumn = c;
                     break;
-                case MAX:
-                    {
-                        int w = (int)(spec[i++]);
-                        lastColumn.setMaxWidth(w);
-                    }
+                case MAX: {
+                    int w = (int)(spec[i++]);
+                    lastColumn.setMaxWidth(w);
+                }
                     break;
-                case MIN:
-                    {
-                        int w = (int)(spec[i++]);
-                        lastColumn.setMinWidth(w);
-                    }
+                case MIN: {
+                    int w = (int)(spec[i++]);
+                    lastColumn.setMinWidth(w);
+                }
                     break;
-                case PREF:
-                    {
-                        int w = (int)(spec[i++]);
-                        lastColumn.setPrefWidth(w);
-                    }
+                case PREF: {
+                    int w = (int)(spec[i++]);
+                    lastColumn.setPrefWidth(w);
+                }
                     break;
                 case ROWS:
                     int n = (int)(spec[i++]);
-                    for(int j=0; j<n; j++) {
+                    for (int j = 0; j < n; j++) {
                         table.getItems().add("");
                     }
                     break;
@@ -433,12 +453,12 @@ public class ATableViewResizeTester extends Application {
                 throw new Error("?" + x);
             }
         }
-        
+
         BorderPane bp = new BorderPane();
         bp.setCenter(table);
         return bp;
     }
-    
+
     protected static class SwingPanel extends JPanel {
         public SwingPanel() {
             super(new BorderLayout());
@@ -447,40 +467,40 @@ public class ATableViewResizeTester extends Application {
         public void updatePane(Policy policy, Object[] spec) {
             JComponent p = createPanel(policy, spec);
             removeAll();
-            if(p != null) {
+            if (p != null) {
                 add(p);
             }
             validate();
             repaint();
         }
-        
+
         private int createHSBPolicy(Policy p) {
-            switch(p) {
-            case AUTO_RESIZE_OFF:
-                return JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED;
+            switch (p) {
             case AUTO_RESIZE_ALL_COLUMNS:
             case AUTO_RESIZE_LAST_COLUMN:
             case AUTO_RESIZE_NEXT_COLUMN:
             case AUTO_RESIZE_SUBSEQUENT_COLUMNS:
             case CONSTRAINED_RESIZE_POLICY:
                 return JScrollPane.HORIZONTAL_SCROLLBAR_NEVER;
+            case UNCONSTRAINED_RESIZE_POLICY:
+                return JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED;
             default:
                 throw new Error("?" + p);
             }
         }
-        
+
         protected JComponent createPanel(Policy policy, Object[] spec) {
-            if((spec == null) || (policy == null)) {
+            if ((spec == null) || (policy == null)) {
                 return null;
             }
-            
+
             DefaultTableModel m = new DefaultTableModel() {
                 @Override
                 public Object getValueAt(int row, int column) {
                     return "";
                 }
             };
-            
+
             JTable t = new JTable(m);
             t.setShowHorizontalLines(true);
             t.setShowVerticalLines(true);
@@ -488,38 +508,35 @@ public class ATableViewResizeTester extends Application {
             t.setAutoResizeMode(policy.getValue());
 
             javax.swing.table.TableColumn lastColumn = null;
-            
-            for(int i=0; i<spec.length; ) {
+
+            for (int i = 0; i < spec.length;) {
                 Object x = spec[i++];
-                if(x instanceof Cmd cmd) {
-                    switch(cmd) {
+                if (x instanceof Cmd cmd) {
+                    switch (cmd) {
                     case COL:
                         javax.swing.table.TableColumn c = new javax.swing.table.TableColumn();
                         t.getColumnModel().addColumn(c);
                         c.setHeaderValue("C" + t.getColumnCount());
                         lastColumn = c;
                         break;
-                    case MAX:
-                        {
-                            int w = (int)(spec[i++]);
-                            lastColumn.setMaxWidth(w);
-                        }
+                    case MAX: {
+                        int w = (int)(spec[i++]);
+                        lastColumn.setMaxWidth(w);
+                    }
                         break;
-                    case MIN:
-                        {
-                            int w = (int)(spec[i++]);
-                            lastColumn.setMinWidth(w);
-                        }
+                    case MIN: {
+                        int w = (int)(spec[i++]);
+                        lastColumn.setMinWidth(w);
+                    }
                         break;
-                    case PREF:
-                        {
-                            int w = (int)(spec[i++]);
-                            lastColumn.setPreferredWidth(w);
-                        }
+                    case PREF: {
+                        int w = (int)(spec[i++]);
+                        lastColumn.setPreferredWidth(w);
+                    }
                         break;
                     case ROWS:
                         int n = (int)(spec[i++]);
-                        for(int j=0; j<n; j++) {
+                        for (int j = 0; j < n; j++) {
                             m.addRow((Object[])null);
                         }
                         break;
@@ -535,13 +552,13 @@ public class ATableViewResizeTester extends Application {
                     throw new Error("?" + x);
                 }
             }
-                        
+
             EmptyBorder b = new EmptyBorder(0, 0, 0, 0);
-            
+
             int hsp = createHSBPolicy(policy);
             JScrollPane scroll = new JScrollPane(t, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, hsp);
             scroll.setBorder(b);
-            
+
             JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, new JPanel());
             split.setContinuousLayout(true);
             return split;
