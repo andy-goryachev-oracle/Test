586ec2946a1ded8c5a26c554b1753ca35d235afa
Andy Goryachev <andy.goryachev@oracle.com>
2022-09-30 09:13:14 -0700

    text field test

--- src/goryachev/apps/LeakTest.java
+++ src/goryachev/apps/LeakTest.java
@@ -1,51 +1,115 @@
 package goryachev.apps;
 
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.skin.SpinnerSkin;
+import javafx.scene.control.Skin;
+import javafx.scene.control.TextField;
+import javafx.scene.control.skin.MenuBarSkin;
+import javafx.scene.control.skin.TextFieldSkin;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
- *
+ * Tests memory leak when replacing the skin.
  */
 public class LeakTest extends Application {
-    private int id;
+    
+    enum Type {
+        MENUBAR,
+        TEXTFIELD
+    }
+    
+    interface Test<T extends Control> {
+        public T createNode();
+        
+        public Skin<T> createSkin(T control);
+    }
     
     public static void main(String[] args) {
         Application.launch(LeakTest.class, args);
     }
-
+    
     @Override
     public void start(final Stage stage) throws Exception {
-        BorderPane rootPane = new BorderPane();
-        
-        Spinner t = new Spinner();
+        // change type
+        Type t =
+//          Type.MENUBAR;
+            Type.TEXTFIELD;
+        createStage(stage, createTest(t));
+    }
+    
+    public Test<?> createTest(Type t) {
+        switch(t) {
+        case MENUBAR:
+            return  new Test<MenuBar>() {
+                @Override
+                public MenuBar createNode() {
+                    MenuBar b = new MenuBar();
+                    Menu m = new Menu("menu");
+                    b.getMenus().add(m);
+                    m.getItems().add(new MenuItem("item 1"));
+                    m.getItems().add(new MenuItem("item 2"));
+                    m.getItems().add(new MenuItem("item 3"));
+                    
+                    m = new Menu("menu");
+                    b.getMenus().add(m);
+                    Menu m2;
+                    m.getItems().add(m2 = new Menu("item 1"));
+                    m.getItems().add(new MenuItem("item 2"));
+                    m.getItems().add(new MenuItem("item 3"));
+                    
+                    m2.getItems().add(new MenuItem("item 21"));
+                    m2.getItems().add(new MenuItem("item 22"));
+                    m2.getItems().add(new MenuItem("item 23"));
+                    return b;
+                }
 
-        Button testButton = new Button("Replace Skin");
-        testButton.setOnAction(e -> {
-            t.setSkin(new AA(t));
+                @Override
+                public Skin<MenuBar> createSkin(MenuBar control) {
+                    class AA_MenuBarSkin extends MenuBarSkin {
+                        public AA_MenuBarSkin(MenuBar control) {
+                            super(control);
+                        }
+                    }
+                    return new AA_MenuBarSkin(control);
+                }
+            };
+        case TEXTFIELD:
+            return new Test<TextField>() {
+                @Override
+                public TextField createNode() {
+                    return new TextField("yo");
+                }
+
+                @Override
+                public Skin<TextField> createSkin(TextField control) {
+                    return new TextFieldSkin(control);
+                }
+            };
+        default:
+            throw new Error("?" + t);
+        }
+    }
+    
+    protected <T extends Control> void createStage(Stage stage, Test<T> test) {
+        T c = test.createNode();
+         
+        Button button = new Button("Replace Skin");
+        button.setOnAction(e -> {
+            c.setSkin(test.createSkin(c));
         });
-        rootPane.setTop(testButton);
-        rootPane.setCenter(new ScrollPane(t));
+        
+        BorderPane rootPane = new BorderPane();
+        rootPane.setTop(c);
+        rootPane.setBottom(button);
         stage.setScene(new Scene(rootPane, 800, 600));
-        stage.setTitle("JDK-8293444 " + System.getProperty("java.version"));
+        stage.setTitle("Memory Leak Test " + System.getProperty("java.version"));
         stage.show();
-    }
-    
-    public static class AndyScrollPane extends ScrollPane {
-        public AndyScrollPane(Node c) {
-            super(c);
-        }
-    }
-    
-    protected static class AA extends SpinnerSkin {
-        public AA(Spinner control) {
-            super(control);
-        }
     }
 }
