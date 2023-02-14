a91ab4f8006ea65f11c790393a85d919d9358f24
Andy Goryachev <andy.goryachev@oracle.com>
2023-02-13 16:47:58 -0800

    double diacritics

--- src/goryachev/monkey/pages/TextAreaPage.java
+++ src/goryachev/monkey/pages/TextAreaPage.java
@@ -190,7 +190,9 @@ public class TextAreaPage extends TestPaneBase {
         case UNICODE:
             return generateTextForWritingSystems();
         case COMBINING:
-            return "Tibetan HAKṢHMALAWARAYAṀ: \u0f67\u0f90\u0fb5\u0fa8\u0fb3\u0fba\u0fbc\u0fbb\u0f82\n(U+0f67 U+0f90 U+0fb5 U+0fa8 U+0fb3 U+0fba U+0fbc U+0fbb U+0f82)";
+            return
+                "Tibetan HAKṢHMALAWARAYAṀ: \u0f67\u0f90\u0fb5\u0fa8\u0fb3\u0fba\u0fbc\u0fbb\u0f82\n(U+0f67 U+0f90 U+0fb5 U+0fa8 U+0fb3 U+0fba U+0fbc U+0fbb U+0f82)\n" +
+                "Double diacritics: a\u0360b a\u0361b a\u0362b a\u035cb";
         default:
             return "?" + ch;
         }
