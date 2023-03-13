94bc6baf266653a2929095fd809ec139bdfe05f6
Andy Goryachev <andy.goryachev@oracle.com>
2023-03-13 09:56:00 -0700

    sql demo

--- src/goryachev/apps/rich/Models.java
+++ src/goryachev/apps/rich/Models.java
@@ -32,7 +32,8 @@ public enum Models {
     DEMO("Demo"),
     EDITABLE_PLAIN("Editable Plaintext Model"),
     EDITABLE_STYLED("Editable Styled Model"),
-    NOTEBOOK("Notebook"),
+    NOTEBOOK("Notebook: Embedded Chart"),
+    NOTEBOOK2("Notebook: SQL Queries"),
     MONOSPACED("Monospaced"),
     NULL("null"),
     TABS("Tabs"),
@@ -81,6 +82,8 @@ public enum Models {
             return new DemoStyledTextModel(2_000_000_000, true);
         case NOTEBOOK:
             return new NotebookModel();
+        case NOTEBOOK2:
+            return new NotebookModel2();
         case NULL:
             return null;
         case ONE_LINE:
