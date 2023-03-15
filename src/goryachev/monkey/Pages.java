35cd699cfaf4d1d91a90b2cd69a66217cd6c8f01
Andy Goryachev <andy.goryachev@oracle.com>
2023-03-15 11:05:57 -0700

    spinner page

--- src/goryachev/monkey/Pages.java
+++ src/goryachev/monkey/Pages.java
@@ -33,24 +33,46 @@ public class Pages {
     public static DemoPage[] create() {
         return new DemoPage[] {
             new DemoPage("Accordion", AccordionPage::new),
+            // TODO Button
+            // TODO ButtonBar
             new DemoPage("Canvas", CanvasPage::new),
             new DemoPage("Chart", ChartPage::new),
+            // TODO CheckBox
             new DemoPage("ChoiceBox", ChoiceBoxPage::new),
             new DemoPage("ComboBox", ComboBoxPage::new),
-            new DemoPage("Dual Focus JDK-8292933", DualFocusPage::new),
-            new DemoPage("DatePicker in Alert", DatePickerPage::new),
+            // ColorPicker
             new DemoPage("HtmlEditor", HtmlEditorPage::new),
+            // TODO Hyperlink
+            // TODO InputField: DoubleField, IntegerField, WebColorField
             new DemoPage("Label", LabelPage::new),
             new DemoPage("ListView", ListViewPage::new),
+            // TODO MenuBar
+            // TODO MenuButton
+            // TODO Pagination
+            // TODO ProgressIndicator
             new DemoPage("ScrollBar", ScrollBarPage::new),
+            // TODO ScrollPane
+            // TODO Separator
+            // TODO Slider
+            new DemoPage("Spinner", SpinnerPage::new),
+            // TODO SplitPane
             new DemoPage("TableView", TableViewPage::new),
+            // TODO TabPane
             new DemoPage("Text", TextPage::new),
             new DemoPage("TextArea", TextAreaPage::new),
             new DemoPage("TextField", TextFieldPage::new),
             new DemoPage("TextFlow", TextFlowPage::new),
-            new DemoPage("TreeView", TreeViewPage::new),
+            // TODO TitledPane
+            // TODO ToggleButton
+            // TODO ToolBar
+            // TODO in tables: Cell, DateCell, IndexedCell* ?
             new DemoPage("TreeTableView", TreeTableViewPage::new),
-            new DemoPage("native2ascii", Native2AsciiPage::new),
+            new DemoPage("TreeView", TreeViewPage::new),
+            // bugs
+            new DemoPage("⚑ Dual Focus JDK-8292933", DualFocusPage::new),
+            new DemoPage("⚑ DatePicker in Alert", DatePickerPage::new),
+            // tools
+            new DemoPage("⚒ native2ascii", Native2AsciiPage::new),
         };
     }
 }
