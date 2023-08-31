8c084748828c6cf8f8a9ee7403226b7813ba28bf
Andy Goryachev <andy.goryachev@oracle.com>
2023-08-31 14:20:35 -0700

    swing bugs

--- src/goryachev/apps/AppTestLauncher.java
+++ src/goryachev/apps/AppTestLauncher.java
@@ -34,7 +34,8 @@ import goryachev.bugs.*;
  * (Remove main(String) from the test snippet)
  */
 public class AppTestLauncher {
-    public static void main(String[] args) {
-        Application.launch(SwingNodeLeakDemo_8262518.class, args);
+    public static void main(String[] args) throws Throwable {
+        JFXPanel_NPE_8255248.main(args);
+        //Application.launch(SwingNodeLeakDemo_8262518.class, args);
     }
 }
