2bc95915d45f727e5f184977b677fab1a058ce8b
Andy Goryachev <andy.goryachev@oracle.com>
2023-10-05 12:35:00 -0700

    swing node

--- src/goryachev/apps/AppTestLauncher.java
+++ src/goryachev/apps/AppTestLauncher.java
@@ -42,6 +42,6 @@ public class AppTestLauncher {
 //        System.setProperty("prism.showdirty", "true");
         
         //JFXPanel_NPE_8255248.main(args);
-        Application.launch(RobotKeypadFail_8316307.class, args);
+        Application.launch(SwingNode_Resize_8154846.class, args);
     }
 }
