802bac17f3bf2aa27ef116e2f140c3cf8faa02f8
Andy Goryachev <andy.goryachev@oracle.com>
2023-10-20 09:46:14 -0700

    tree table view

--- src/goryachev/apps/AppTestLauncher.java
+++ src/goryachev/apps/AppTestLauncher.java
@@ -42,6 +42,6 @@ public class AppTestLauncher {
 //        System.setProperty("prism.showdirty", "true");
         
         //JFXPanel_NPE_8255248.main(args);
-        Application.launch(TextArea_Aramaic_8318099.class, args);
+        Application.launch(TreeTableView_8187314.class, args);
     }
 }
