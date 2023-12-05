a6d44d8d270e9d47b1343cf0ee34166bbeb1b020
Andy Goryachev <andy.goryachev@oracle.com>
2023-12-05 08:08:51 -0800

    tree table selection

--- src/goryachev/apps/AppTestLauncher.java
+++ src/goryachev/apps/AppTestLauncher.java
@@ -42,6 +42,6 @@ public class AppTestLauncher {
 //        System.setProperty("prism.showdirty", "true");
         
         //JFXPanel_NPE_8255248.main(args);
-        Application.launch(TextField_IME_8320912.class, args);
+        Application.launch(TreeTable_Select_8321323.class, args);
     }
 }
