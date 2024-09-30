8d193a56c1843dc5fb2d14b9c245d9586896d394
Andy Goryachev <andy.goryachev@oracle.com>
2024-09-30 09:19:10 -0700

    scroll pane

--- src/goryachev/research/FocusPolicyResearch.java
+++ src/goryachev/research/FocusPolicyResearch.java
@@ -1,13 +1,16 @@
 package goryachev.research;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
+ * Researching focus-related issues.
  */
 public class FocusPolicyResearch extends Application {
 
@@ -17,6 +20,17 @@ public class FocusPolicyResearch extends Application {
         stage.setWidth(450);
         stage.setHeight(550);
         
+        VBox p = new VBox(
+            createPane(),
+            new ScrollPane(createPane())
+        );
+
+        Scene scene = new Scene(p);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    Node createPane() {
         ToggleButton t1 = new ToggleButton("1");
         ToggleButton t2 = new ToggleButton("2");
         ToggleButton t3 = new ToggleButton("3");
@@ -28,15 +42,11 @@ public class FocusPolicyResearch extends Application {
             t3
         );
 
-        VBox vb = new VBox(
+        return new VBox(
             t1,
             t2,
             t3,
             new Button("Button")
         );
-
-        Scene scene = new Scene(vb);
-        stage.setScene(scene);
-        stage.show();
     }
 }
