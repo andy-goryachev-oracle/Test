32f15013150cc076187f73c179618d7f2b5e1760
Andy Goryachev <andy.goryachev@oracle.com>
2025-01-06 12:02:06 -0800

    tree view

--- src/goryachev/apps/AppTestLauncher.java
+++ src/goryachev/apps/AppTestLauncher.java
@@ -35,7 +35,7 @@ import goryachev.tests.*;
 public class AppTestLauncher {
     public static void main(String[] args) throws Throwable {
         // enableLogging();
-        Application.launch(Menu_RenderScale_8346281.class, args);
+        Application.launch(TreeView_RenderingFailure_8346824.class, args);
     }
 
     private static void enableLogging() {
