625855f6d48ea572e3a9cfdf2f8864f1b9236782
Andy Goryachev <andy.goryachev@oracle.com>
2025-05-16 08:27:21 -0700

    swing egyptian

---
+++ src/goryachev/research/JTextArea_Egyptian.java
@@ -0,0 +1,33 @@
+package goryachev.research;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Panel;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+/**
+ *
+ */
+public class JTextArea_Egyptian {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            new JTextArea_Egyptian().startEDT();
+        });
+    }
+
+    private void startEDT() {
+        // 𓅂𓁹𓁋𓁨
+        JTextArea t = new JTextArea("\ud80c\udd42\ud80c\udc79\ud80c\udc4b\ud80c\udc68");
+        
+        Panel p = new Panel(new BorderLayout());
+        p.add(t);
+
+        JFrame f = new JFrame();
+        f.setTitle("Ancient Egyptian 𓅂𓁹𓁋𓁨");
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.setSize(400, 300);
+        f.add(p);
+        f.setVisible(true);
+    }
+}
