2b6b6e15bd725782944fba2c375353fedb0e188e
Andy Goryachev <andy.goryachev@oracle.com>
2025-06-12 11:58:15 -0700

    stage full screen

---
+++ src/goryachev/bugs/Stage_FullScreen_Extended.java
@@ -0,0 +1,30 @@
+package goryachev.bugs;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * 
+ */
+public class Stage_FullScreen_Extended extends Application {
+    @Override
+    public void start(Stage stage) {
+        //var headerBar = new HeaderBar();
+        var root = new BorderPane();
+        root.setTop(new Label("yo")); //headerBar);
+        
+        Button btnFullScreen = new Button("FullScreen");
+        btnFullScreen.setOnAction((_) -> stage.setFullScreen(!stage.isFullScreen()));
+        root.setCenter(btnFullScreen);
+
+        stage.setScene(new Scene(root, 640, 480));
+        //stage.initStyle(StageStyle.EXTENDED);
+        //stage.setResizable(false);
+        stage.show();
+    }
+}
