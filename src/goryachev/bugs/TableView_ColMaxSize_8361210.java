0c9c91362fd8fd7ff1e586b2da8d3370ae1845ab
Andy Goryachev <andy.goryachev@oracle.com>
2025-07-01 09:23:16 -0700

    jbs

--- src/goryachev/bugs/TableView_ColMaxSize_8361210.java
+++ src/goryachev/bugs/TableView_ColMaxSize_8361210.java
@@ -8,14 +8,17 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class TableView_ColMaxSizeBug extends Application {
+/**
+ * https://bugs.openjdk.org/browse/JDK-8361210
+ */
+public class TableView_ColMaxSize_8361210 extends Application {
+    
     @Override
     public void start(Stage stage) {
         TableColumn<String, String> col1 = new TableColumn<>();
         col1.setGraphic(new Text("Column One (... ...)"));
         
         // Setting the max width causes this column to shrink unexpectedly
-        // 
         col1.setMaxWidth(500);
         //col1.setPrefWidth(500);
 
@@ -28,7 +31,9 @@ public class TableView_ColMaxSizeBug extends Application {
             new TableColumn<>("Col 4"),
             new TableColumn<>("Col 5")
         );
+        
         stage.setScene(new Scene(new VBox(table), 600, 200));
+        stage.setTitle("JDK-8361210");
         stage.show();
     }
 }
