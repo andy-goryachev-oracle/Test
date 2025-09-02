1a88806544c6e7d591ba558d9acf0278d42ba574
andy goryachev (win) <andy.goryachev@oracle.com>
2025-09-02 13:24:18 -0700

    toolbar

--- src/goryachev/bugs/Toolbar_FractionalScale_8364049_8364088.java
+++ src/goryachev/bugs/Toolbar_FractionalScale_8364049_8364088.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -16,9 +17,20 @@ import javafx.stage.Stage;
 public class Toolbar_FractionalScale_8364049_8364088 extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
+        ToolBar tb = new ToolBar(
+            new Separator(Orientation.VERTICAL),
+            new Button("Create Schema")
+        );
+
+        BorderPane bp = new BorderPane();
+        bp.setTop(new HBox(tb));
+        primaryStage.setScene(new Scene(bp, 600, 400));
+        primaryStage.show();
+    }
+    
+    public void start2(Stage stage) {
         ToolBar tb = new ToolBar(new Button("Create Schema"));
-//        tb.setOrientation(Orientation.VERTICAL);
 
         BorderPane bp = new BorderPane();
         bp.setTop(new HBox(tb));
