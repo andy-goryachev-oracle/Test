80e56665dc87c96ead741f67f8ee4592ac42c5a0
Andy Goryachev <andy.goryachev@oracle.com>
2025-12-09 10:49:01 -0800

    rich paragraph builder api

--- src/goryachev/apps/AppTestLauncher.java
+++ src/goryachev/apps/AppTestLauncher.java
@@ -11,7 +11,7 @@ import goryachev.tests.*;
 public class AppTestLauncher {
     public static void main(String[] args) throws Throwable {
         // enableLogging();
-        Application.launch(Clipboard_WrongType_8269630.class, args);
+        Application.launch(RichTextArea_CustomModel_8366198.class, args);
     }
 
     private static void enableLogging() {
--- src/goryachev/research/RichTextArea_CustomModel_8366198.java
+++ src/goryachev/research/RichTextArea_CustomModel_8366198.java
@@ -3,6 +3,7 @@ package goryachev.research;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import jfx.incubator.scene.control.richtext.RichTextArea;
 import jfx.incubator.scene.control.richtext.TextPos;
@@ -10,33 +11,76 @@ import jfx.incubator.scene.control.richtext.model.RichParagraph;
 import jfx.incubator.scene.control.richtext.model.RichTextModel;
 import jfx.incubator.scene.control.richtext.model.StyleAttribute;
 import jfx.incubator.scene.control.richtext.model.StyleAttributeMap;
+import jfx.incubator.scene.control.richtext.model.StyledSegment;
 
 /**
  * https://bugs.openjdk.org/browse/JDK-8366198
  */
 public class RichTextArea_CustomModel_8366198 extends Application {
-    public static final StyleAttribute<Boolean> RED_SQUIGGLY = new StyleAttribute<>("RED_SQUIGGLY", Boolean.class, false);
-    public static final StyleAttribute<Boolean> BLUE_SQUIGGLY = new StyleAttribute<>("BLUE_SQUIGGLY", Boolean.class, false);
+    private static final StyleAttribute<Boolean> RED_SQUIGGLY = new StyleAttribute<>("RED_SQUIGGLY", Boolean.class, false);
+    private static final StyleAttribute<Boolean> BLUE_SQUIGGLY = new StyleAttribute<>("BLUE_SQUIGGLY", Boolean.class, false);
+    private static final Color RED = Color.RED.deriveColor(0.0, 1.0, 1.0, 0.5);
+    private static final Color BLUE = Color.BLUE.deriveColor(0.0, 1.0, 1.0, 0.3);
     
     @Override
     public void start(Stage stage) throws Exception {
         
         RichTextModel m = new RichTextModel() {
             @Override
-            public RichParagraph getParagraph(int index) {
-                RichParagraph p = super.getParagraph(index);
-                applyCustomHighlights(p);
-                 return p;
+            protected RichParagraph.Builder buildParagraph(int index) {
+                RichParagraph.Builder b = super.buildParagraph(index);
+                applyCustomHighlights(b);
+                return b;
             }
             
             // so here we could iterate over text segments looking for the special attributes (expensive!)
             // to determine the start/end positions, then add highlights to the paragraph.
-            private void applyCustomHighlights(RichParagraph p) {
-                // TODO needs JDK-8371070
+            // the main problem here is that style attributes are specific to segments, and the highlights
+            // might span segments or be applied to partial segments (that is, they are conceptually separate
+            // from the segments).
+            private void applyCustomHighlights(RichParagraph.Builder b) {
+                int red = -1;
+                int blue = -1;
+                int ix = 0;
+                int ct = b.getSegmentCount();
+                for (int i = 0; i < ct; i++) {
+                    // NOTE: requires new APIs in RichParagraph.Builder
+                    StyledSegment seg = b.getSegment(i);
+                    // style resolver is not needed at this point
+                    StyleAttributeMap a = seg.getStyleAttributeMap(null);
+                    if (a.contains(RED_SQUIGGLY)) {
+                        if (red < 0) {
+                            red = ix;
+                        }
+                    } else {
+                        if (red >= 0) {
+                            b.addWavyUnderline(red, ix, RED);
+                            red = -1;
+                        }
+                    }
+                    if (a.contains(BLUE_SQUIGGLY)) {
+                        if (blue < 0) {
+                            blue = ix;
+                        }
+                    } else {
+                        if (blue >= 0) {
+                            b.addWavyUnderline(blue, ix, BLUE);
+                            blue = -1;
+                        }
+                    }
+                    ix += seg.getText().length();
+                }
+                if (red >= 0) {
+                    b.addWavyUnderline(red, ix, RED);
+                }
+                if (blue >= 0) {
+                    b.addWavyUnderline(blue, ix, BLUE);
+                }
             }
         };
         
         RichTextArea r = new RichTextArea(m);
+        r.setWrapText(true);
 
         r.appendText("""
             red squiggly
@@ -46,11 +90,13 @@ public class RichTextArea_CustomModel_8366198 extends Application {
             """);
         r.select(TextPos.ofLeading(0, 1));
         r.applyStyle(TextPos.ZERO, TextPos.ofLeading(1, 100), StyleAttributeMap.of(RED_SQUIGGLY, true));
+        r.applyStyle(TextPos.ofLeading(0, 6), TextPos.ofLeading(0, 100), StyleAttributeMap.of(StyleAttributeMap.BOLD, true));
         r.applyStyle(TextPos.ofLeading(1, 0), TextPos.ofLeading(2, 100), StyleAttributeMap.of(BLUE_SQUIGGLY, true));
 
         BorderPane pane = new BorderPane();
         pane.setCenter(r);
         stage.setScene(new Scene(pane));
+        stage.setTitle("Better Highlights APIs");
         stage.show();
     }
 }
